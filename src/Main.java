e2e1de8a4afa2dcc953085eb849ddb7d78bf39ad
Agnohendrix <agnohendrix@gmail.com>
2023-03-29 19:54:24 +0200

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(Runtime.getRuntime().availableProcessors() + " cores available");
+    }
+}
